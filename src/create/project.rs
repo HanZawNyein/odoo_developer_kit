3b89902dd11fe3ce809ff6194eb443c5b9b0dc8b
HanZawNyein <hanzawnyineonline@gmail.com>
2026-07-09 01:21:48 +0630

    [FIX] release

--- src/create/project.rs
+++ src/create/project.rs
@@ -1,6 +1,6 @@
-use std::fs;
 use std::path::{Path, PathBuf};
 use std::time::Duration;
+use std::{env, fs};
 
 use anyhow::Result;
 use indicatif::ProgressBar;
@@ -12,14 +12,16 @@ use crate::error::OdkError;
 use crate::utils::command::run_required;
 
 pub fn create_project(config: &ProjectConfig) -> Result<PathBuf> {
-    let target = PathBuf::from(&config.project_path);
+    let target = absolute_path(&PathBuf::from(&config.project_path))?;
     ensure_target_is_available(&target)?;
 
-    run_step("Cloning repository", || {
-        git::clone_repository(&config.git_repository, &target)
-    })?;
+    run_step(
+        "Cloning repository",
+        Some(config.git_repository.clone()),
+        || git::clone_repository(&config.git_repository, &target),
+    )?;
 
-    run_step("Installing Python with uv", || {
+    run_step("Installing Python with uv", None, || {
         let args = vec![
             "python".to_owned(),
             "install".to_owned(),
@@ -28,30 +30,38 @@ pub fn create_project(config: &ProjectConfig) -> Result<PathBuf> {
         run_required("uv", &args, Some(&target))
     })?;
 
-    run_step("Creating virtual environment with uv", || {
-        let args = vec![
-            "venv".to_owned(),
-            ".venv".to_owned(),
-            "--python".to_owned(),
-            config.python_version.clone(),
-        ];
-        run_required("uv", &args, Some(&target))
-    })?;
+    run_step(
+        "Creating virtual environment with uv",
+        Some(target.join(".venv").display().to_string()),
+        || {
+            let args = vec![
+                "venv".to_owned(),
+                ".venv".to_owned(),
+                "--python".to_owned(),
+                config.python_version.clone(),
+            ];
+            run_required("uv", &args, Some(&target))
+        },
+    )?;
 
     create_project_directories(&target, config.use_docker)?;
 
     let renderer = TemplateRenderer::new()?;
     renderer.render_project(config, &target)?;
 
-    run_step("Generating uv.lock", || {
-        let args = vec!["lock".to_owned()];
-        run_required("uv", &args, Some(&target))
-    })?;
+    run_step(
+        "Generating uv.lock",
+        Some(target.join("uv.lock").display().to_string()),
+        || {
+            let args = vec!["lock".to_owned()];
+            run_required("uv", &args, Some(&target))
+        },
+    )?;
 
     Ok(target)
 }
 
-fn run_step<F>(message: &'static str, action: F) -> Result<()>
+fn run_step<F>(message: &'static str, success_detail: Option<String>, action: F) -> Result<()>
 where
     F: FnOnce() -> Result<(), OdkError>,
 {
@@ -61,13 +71,25 @@ where
 
     match action() {
         Ok(()) => {
-            progress.finish_with_message(format!("{message} done"));
+            let done_message = match success_detail {
+                Some(detail) => format!("{message} done - {detail}"),
+                None => format!("{message} done"),
+            };
+            progress.finish_with_message(done_message);
             Ok(())
         }
         Err(error) => {
             progress.abandon_with_message(format!("{message} failed"));
             Err(error.into())
         }
+    }
+}
+
+fn absolute_path(path: &Path) -> Result<PathBuf, OdkError> {
+    if path.is_absolute() {
+        Ok(path.to_path_buf())
+    } else {
+        Ok(env::current_dir()?.join(path))
     }
 }
 
